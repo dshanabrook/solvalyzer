388273b8fb6c01e0eece6c9375f7d4ee4fc37497
David Shanabrook <davidshanabrook@me.com>
2012-07-10 12:20:52 -0400

    Changed target name to touchMath

---
+++ touchMath/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  touchMath
+//
+//  Created by david hilton shanabrook on 7/10/12.
+//  Copyright (c) 2012 Diabolical Labs, LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
